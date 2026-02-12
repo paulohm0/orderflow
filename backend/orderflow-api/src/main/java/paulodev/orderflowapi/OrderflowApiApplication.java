0ae2b975895459d1520ab248f8dfe12a74d3328b
Paulo Henrique <paulohenrique1019@gmail.com>
2026-02-12 17:00:56 -0300

    [backend][chore] add rabbitMQ, Postgres and Docker config

---
+++ backend/orderflow-api/src/main/java/paulodev/orderflowapi/OrderflowApiApplication.java
@@ -0,0 +1,13 @@
+package paulodev.orderflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderflowApiApplication.class, args);
+    }
+
+}
